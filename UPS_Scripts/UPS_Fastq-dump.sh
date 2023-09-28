b93829afd688683cd576fd1f3b8381349f04187b
SLB-bioinformatics <137393100+SLB-bioinformatics@users.noreply.github.com>
2023-09-28 16:29:43 +0100

    Delete UPS_Scripts directory

--- UPS_Scripts/UPS_Fastq-dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=Fastq-dump
-module load SRA
-SSRPath="$1"
-echo "$SSRPath"
-fastq-dump --gzip --skip-technical --readids --read-filter pass --dumpbase --split-3 --clip "$SSRPath"
-
-
